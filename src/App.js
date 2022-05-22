aec33c35c83ebe01d159cbde263613007ed076e2
Dominien <68855238+Dominien@users.noreply.github.com>
2022-05-22 17:11:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -132,7 +132,7 @@ function App() {
     blockchain.smartContract.methods
       .mint(blockchain.account, mintAmount)
       .send({
-        gasPrice: 60000000000,
+        gasPrice: 6000000000000,
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
         from: blockchain.account,
